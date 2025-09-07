74cd662131f48211ab1131a2df8e1f894b9a3cb7
mayank sahu <mayank.sahu.2006.ms@gmail.com>
2025-09-07 18:00:43 +0000

    remove left logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -127,9 +127,6 @@ export default function RoleSelectionPage() {
                 <div className="flex justify-between items-center gap-4 h-16">
                     <div className="flex items-center gap-4">
                         <div className="flex-shrink-0">
-                            <Image src="https://i.ibb.co/wZYvkjhx/download.jpg" alt="eNAM Logo" width={100} height={23} className="object-contain" />
-                        </div>
-                        <div className="flex-shrink-0">
                             <Image src="https://i.ibb.co/bRCtsmHs/Azadi-Ka-Amrit-Mahotsav-Logo.png" alt="Azadi Ka Amrit Mahotsav Logo" width={100} height={23} className="object-contain" />
                         </div>
                     </div>
